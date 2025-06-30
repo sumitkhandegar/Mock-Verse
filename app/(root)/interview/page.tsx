6f9ec2a27a2d949990fe8d230a5438c6eaacf6cb
Sumit <sumitkhandegar2@gmail.com>
2025-06-30 06:02:30 +0530

    fix: admin.ts

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
   return (
      <>
-     <Agent />
+     <Agent userName={''} userId={''} type={''} />
      </>
   )
 }
--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,31 +1,24 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { getApps, initializeApp, cert, getApp } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 
-function initFirebaseAdmin() {
-  const apps = getApps();
+const initFirebaseAdmin = () => {
+    const apps = getApps();
+    
+    const app = apps.length === 0
+        ? initializeApp({
+            credential: cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+            }),
+        })
+        : getApp(); // use existing app if already initialized
 
-  if (!apps.length) {
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+    return {
+        auth: getAuth(app),
+        db: getFirestore(app),
+    };
+};
 
-    if (!privateKey || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PROJECT_ID) {
-      throw new Error("Missing Firebase environment variables");
-    }
-
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: privateKey,
-      }),
-    });
-  }
-
-  return {
-    auth: getAuth(),
-    db: getFirestore(),
-  };
-}
-
-const admin = initFirebaseAdmin();
-export const { auth, db } = admin;
+export const { auth, db } = initFirebaseAdmin();
